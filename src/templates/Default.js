72ca9017a3004c3eacf3181606d68b0e368c13a4
Hamilton Tumenas Borges <hamilton.borges@sou.unifeob.edu.br>
2022-01-27 19:44:39 -0300

    adicionando templatedefault, header e user dasboard

---
+++ src/templates/Default.js
@@ -0,0 +1,11 @@
+import Header from '../components/Header'
+
+const Default = ({ children }) => {
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    )
+}
+export default Default
