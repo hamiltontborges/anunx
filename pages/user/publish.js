d53ec878d1b3a43ce1b17b47a24136d3aaf5a467
Hamilton Tumenas Borges <hamilton.borges@sou.unifeob.edu.br>
2022-01-29 18:04:09 -0300

    adicionando box de preco

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -2,7 +2,11 @@ import {
     Box,
     Button,
     Container,
+    FormControl,
     IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
     Select,
     TextField,
     Typography,
@@ -211,6 +215,23 @@ const Publish = () => {
                         variant="outlined"
                         fullWidth
                     />
+                </Box>
+            </Container>
+
+            <Container maxWidth="md" className={classes.boxContainer}>
+                <Box className={classes.box}>
+                    <Typography component="h6" variant="h6" color="textPrimary">
+                        Preço
+                    </Typography>
+                    <br />
+                    <FormControl fullWidth variant="outlined">
+                        <InputLabel>Valor</InputLabel>
+                        <OutlinedInput 
+                            onChange={() => {}}
+                            startAdornment={<InputAdornment position="start">R$</InputAdornment>}
+                            labelWidth={40}
+                        />
+                    </FormControl>
                 </Box>
             </Container>
 
