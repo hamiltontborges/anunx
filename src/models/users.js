0ba5849ec72d3a7b5551a41a39e4fbbca65b1567
Hamilton Tumenas Borges <hamilton.borges@sou.unifeob.edu.br>
2022-02-03 18:35:45 -0300

    sistema de login com email e senha (nextauth)

--- src/models/users.js
+++ src/models/users.js
@@ -15,4 +15,4 @@ const schema = new mongoose.Schema({
   },
 })
 
-export default mongoose.model.users || mongoose.model('users', schema)
+export default mongoose.models.users || mongoose.model('users', schema)
